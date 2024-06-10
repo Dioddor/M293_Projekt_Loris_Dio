da6f2ddc8d19b4203550074d8aeef0a869a25c0f
Lorisimbrogno <142780981+Lorisimbrogno@users.noreply.github.com>
2024-06-10 09:04:45 +0200

    Update Automatisierung.bat

--- Übungen/Loris/T2/Automatisierung.bat
+++ Übungen/Loris/T2/Automatisierung.bat
@@ -1,7 +1,25 @@
 @echo off
-pause
-@ftp www.loris.bplaced.net
+setlocal
 
-username
-password
-pause
+:: Konfigurationsabschnitt
+set SERVER=ftp.bplaced.net
+set PORT=21
+set USERNAME=loris.internett@gmail.com
+set PASSWORD=niger1312
+set LOCAL_PATH=C:\path\to\your\project
+set REMOTE_PATH=/htdocs
+
+:: Skriptabschnitt
+echo open %SERVER% %PORT% > ftpcmd.dat
+echo user %USERNAME% %PASSWORD% >> ftpcmd.dat
+echo binary >> ftpcmd.dat
+echo lcd %LOCAL_PATH% >> ftpcmd.dat
+echo cd %REMOTE_PATH% >> ftpcmd.dat
+echo mput * >> ftpcmd.dat
+echo bye >> ftpcmd.dat
+
+ftp -s:ftpcmd.dat
+
+del ftpcmd.dat
+
+endlocal
